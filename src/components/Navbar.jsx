e83cc1a0e02c362cf7f6db46d42e3b136c14394a
Emerson <emerson.day@gmail.com>
2022-11-07 14:28:54 -0800

    added dynamic items count

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
 import { Button, Container, Navbar, Modal } from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { CartContext } from '../CartContext';
 
 function NavbarComponent() {
+  const cart = useContext(CartContext);
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const productsCount = cart.items.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
 
   return (
     <>
@@ -12,7 +19,7 @@ function NavbarComponent() {
         <Navbar.Brand href="/">E-Commerce Store</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Button onClick={handleShow}>Cart 0 Items</Button>
+          <Button onClick={handleShow}>Cart ({productsCount} Items)</Button>
         </Navbar.Collapse>
       </Navbar>
       <Modal show={show} onHide={handleClose}>
